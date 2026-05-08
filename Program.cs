c52e279ebb16aca1851f17831de36ef341bc130e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-05-08 20:32:12 -0300

    mudanca

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.WriteLine("Digite sua Idade");
+int idade= Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 )
+
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Voce é maior de Idade.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Voce e menor de Idade.");
+}
+Console.ResetColor();
+Console.WriteLine("Obrigado");
+
+
+
